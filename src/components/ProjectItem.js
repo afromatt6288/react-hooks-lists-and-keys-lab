c279f350cd15043dd4ed2ea5ccb52bc64eb78974
Matthew Clark <afromatt6288@hotmail.com>
2023-02-21 17:31:03 -0700

    Lab Complete

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const techie = technologies.map((tech) => {
+    return (
+      <span key={tech}>{tech}</span>
+    )
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{techie}</div>
     </div>
   );
 }
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,23 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
+
 function ProjectList({ projects }) {
+  const list = projects.map((project) => {
+    return (
+    <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies}/>
+  )
+})
+  // const list = projects.map(ProjectItem, <ProjectItem key={projects.id}/>)
+  // const list = projects.map((project) => ({ProjectItem}, <ProjectItem key={project.id}))
+ 
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {list}
+      </div>
     </div>
   );
 }
